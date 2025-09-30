cd0b08c26793fe70f59e8aa1c514def3d830a731
Reza Rizki A <rezariski.akbar@gmail.com>
2025-09-30 15:34:53 +0700

    feat(model): add photo accessor to expose public URL for photo

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Doctor extends Model
 {
@@ -32,5 +34,12 @@ class Doctor extends Model
     public function transactions()
     {
         return $this->hasMany(Transaction::class, 'doctor_id');
+    }
+
+    protected function photo(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => $value ? url(Storage::url($value)) : null,
+        );
     }
 }
