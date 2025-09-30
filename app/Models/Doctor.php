671a0f7c76572830c91d72a891f2fd7c58ca9ec2
Reza Rizki A <rezariski.akbar@gmail.com>
2025-09-30 15:26:21 +0700

    feat(model): define $fillable attributes for mass assignment

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'photo',
+        'about',
+        'yoe',
+        'specialist_id',
+        'hospital_id',
+        'gender',
+    ];
 }
