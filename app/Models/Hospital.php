d865d78771ce9ee5f013c0ae6585953838e01d19
Reza Rizki A <rezariski.akbar@gmail.com>
2025-10-01 13:09:03 +0700

    feat(model): add softdeletes, fillable, public function relationship to hospital

--- app/Models/Hospital.php
+++ app/Models/Hospital.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hospital extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'photo',
+        'about',
+        'address',
+        'city',
+        'post_code',
+        'phone',
+    ];
+
+    public function doctors(): HasMany
+    {
+        return $this->hasMany(Doctor::class, 'hospital_id');
+    }
+
+    public function specialists(): BelongsToMany
+    {
+        return $this->belongsToMany(Specialist::class);
+    }
 }
