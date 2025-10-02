8c04dcf546beedb5bbc4388c71d2cafe13963c47
Reza Rizki <rezariski.akbar@gmail.com>
2025-10-02 14:46:46 +0700

    feat: membuat method getallspecialist, untuk mendapatkan semua daftar specialist

--- app/Repositories/SpecialistRepository.php
+++ app/Repositories/SpecialistRepository.php
@@ -2,6 +2,31 @@
 
 namespace App\Repositories;
 
-class SpecialistRepository {
-    
+use App\Models\Specialist;
+
+class SpecialistRepository
+{
+
+    public function getAllSpecialists(array $fields)
+    {
+        return Specialist::select($fields)->latest()->with(['hospitals', 'doctors'])->paginate(10);
+    }
+
+
+    public function getById(int $id, array $fields)
+    {
+        return Specialist::select($fields)
+            ->with([
+                'hospitals' => function ($q) use ($id) {
+                    $q->withCount(['doctors as doctors_count' => function ($q) use ($id) {
+                        $q->where('specialist_id', $id);
+                    }]);
+                },
+                'doctors' => function ($q) use ($id) {
+                    $q->where('specialist_id', $id)
+                        ->with('hospital:id,name,city,post_code');
+                }
+            ])
+            ->findOrFail($id);
+    }
 }
