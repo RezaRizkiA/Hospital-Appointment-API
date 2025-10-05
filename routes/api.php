acbfa55fadf4c257ae0889348d46c0a0e80cf63c
Reza Rizki A <rezariski.akbar@gmail.com>
2025-10-06 02:32:01 +0700

    feat: Add API routes for transactions and hospital specialists

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use App\Http\Controllers\DoctorController;
 use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\HospitalSpecialistController;
 use App\Http\Controllers\SpecialistController;
+use App\Http\Controllers\TransactionController;
+use App\Models\HospitalSpecialist;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +16,10 @@ Route::get('/user', function (Request $request) {
 Route::apiResource('specialists', SpecialistController::class);
 Route::apiResource('hospitals', HospitalController::class);
 Route::apiResource('doctors', DoctorController::class);
+
+Route::post('hospitals/{hospital}/specialist', [HospitalSpecialistController::class, 'attach']);
+Route::delete('hospitals/{hospital}/specialist/{specialist}', [HospitalSpecialistController::class, 'detach']);
+
+Route::apiResource('transactions', TransactionController::class);
+Route::patch('transactions/{transaction}/status', [TransactionController::class, 'updateStatus']);
+    
