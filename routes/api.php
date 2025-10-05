dc865e1c49f16169fef01713c178aa0016e75dad
Reza Rizki A <rezariski.akbar@gmail.com>
2025-10-06 01:54:11 +0700

    feat: membuat route specialist, hospitals, doctors

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\SpecialistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('specialists', SpecialistController::class);
+Route::apiResource('hospitals', HospitalController::class);
+Route::apiResource('doctors', DoctorController::class);
