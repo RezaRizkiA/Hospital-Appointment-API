4304c60066a3cfc8ddc00cd86a0d6048c4e1914e
Reza Rizki <rezariski.akbar@gmail.com>
2025-10-08 06:00:28 +0700

    feat: membuat dependency injection di dalam HospitalService

--- app/Services/HospitalService.php
+++ app/Services/HospitalService.php
@@ -2,5 +2,14 @@
 
 namespace App\Services;
 
+use App\Repositories\HospitalRepository;
 
-class HospitalService {}
+class HospitalService {
+
+    private $hospitalRepository;
+
+    public function __construct(HospitalRepository $hospitalRepository)
+    {
+        $this->hospitalRepository = $hospitalRepository;
+    }
+}
