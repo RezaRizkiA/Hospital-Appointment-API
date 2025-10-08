e95d23502321006e862cd6d06f584fa77c080252
ezaelakbar <ezarizkisap@gmail.com>
2025-10-08 18:30:11 +0700

    feat: membuat backend doctor di dalam DoctorRepository

--- app/Repositories/DoctorRepository.php
+++ app/Repositories/DoctorRepository.php
@@ -2,7 +2,12 @@
 
 namespace App\Repositories;
 
+use App\Models\Doctor;
+
 class DoctorRepository
 {
-    
+    public function getAll(array $fields)
+    {
+        return Doctor::select($fields)->latest()->with(['specialist', 'hospital'])->paginate(10);
+    }
 }
