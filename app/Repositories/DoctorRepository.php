b9014fe0c14648b7694290496909f980a317e80b
ezaelakbar <ezarizkisap@gmail.com>
2025-10-08 19:43:57 +0700

    feat: membuat backend doctor di dalam file DoctorRepository: function getall, getbyid, create, update, delete

--- app/Repositories/DoctorRepository.php
+++ app/Repositories/DoctorRepository.php
@@ -3,11 +3,45 @@
 namespace App\Repositories;
 
 use App\Models\Doctor;
+use App\Models\Hospital;
 
 class DoctorRepository
 {
     public function getAll(array $fields)
     {
         return Doctor::select($fields)->latest()->with(['specialist', 'hospital'])->paginate(10);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return Doctor::select($fields)
+            ->with(
+                [
+                    'specialist' => function ($q) use ($id) {
+                        $q->where('specialist_id', $id);
+                    },
+                    'hospital' => function ($q) use ($id) {
+                        $q->where('hospital_id', $id);
+                    }
+                ]
+            )->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return Doctor::create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $hospital = Hospital::findOrFail($id);
+        $hospital->update($data);
+        return $hospital;
+    }
+
+    public function delete(int $id)
+    {
+        $hospital = Hospital::findOrFail($id);
+        $hospital->delete();
     }
 }
