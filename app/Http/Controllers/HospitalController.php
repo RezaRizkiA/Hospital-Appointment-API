c02270124529bbcc661ece1041ac962ba1d2fc98
Reza Rizki <rezariski.akbar@gmail.com>
2025-10-11 18:27:04 +0700

    feat: membuat file HospitalResource dan membuat method getAll di Hospital Service

--- app/Http/Controllers/HospitalController.php
+++ app/Http/Controllers/HospitalController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\HospitalResource;
+use App\Services\HospitalService;
 use Illuminate\Http\Request;
 
 class HospitalController extends Controller
 {
-    //
+    private $hospitalService;
+
+    public function __construct(HospitalService $hospitalService)
+    {
+        $this->hospitalService = $hospitalService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name', 'photo', 'address', 'city', 'phone'];
+        $hospitals = $this->hospitalService->getAll($fields);
+        return response()->json(HospitalResource::collection($hospitals));
+    }
 }
