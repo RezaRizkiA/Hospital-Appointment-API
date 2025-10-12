b169fff6d353c967f09b2c9b384d4dd11026109c
Reza Rizki <rezariski.akbar@gmail.com>
2025-10-13 02:13:29 +0700

    feat: membuat method attach di HospitalSpecialistController, method attachSpecialistToHospital di HospitalSpecialistService, method attachSpecialist di HospitalSpecialistRepository

--- app/Repositories/HospitalSpecialistRepository.php
+++ app/Repositories/HospitalSpecialistRepository.php
@@ -2,7 +2,20 @@
 
 namespace App\Repositories;
 
+use App\Models\Hospital;
+
 class HospitalSpecialistRepository
 {
-    
+
+    /**
+     * Menautkan spesialis ke rumah sakit.
+     *
+     * @param Hospital $hospital
+     * @param int $specialistId
+     */
+    public function attachSpecialist(Hospital $hospital, int $specialistId)
+    {
+        $result = $hospital->specialists()->syncWithoutDetaching([$specialistId]);
+        return !empty($result['attached']);
+    }
 }
