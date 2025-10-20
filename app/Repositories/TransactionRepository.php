773a0fe778b36996de609522ddee49f42714aaa6
Reza Rizki <rezariski.akbar@gmail.com>
2025-10-20 11:26:51 +0700

    feat: membuat method getbyidformanager dan getbyidforuser didalam transactionrepository

--- app/Repositories/TransactionRepository.php
+++ app/Repositories/TransactionRepository.php
@@ -13,11 +13,25 @@ class TransactionRepository
             ->paginate(10);
     }
 
+    public function getByIdForManager(int $id)
+    {
+        return Transaction::with(['doctor', 'doctor.specialist', 'doctor.hospital', 'user'])
+            ->findOrFail($id);
+    }
+
     public function getAllForUser(int $userId)
     {
         return Transaction::where('user_id', $userId)
             ->with(['doctor', 'doctor.specialist', 'doctor.hospital'])
             ->latest()
             ->paginate(10);
+    }
+
+    public function getByIdForUser(int $id, int $userId)
+    {
+        return Transaction::where('id', $id)
+            ->where('user_id', $userId)
+            ->with(['doctor', 'doctor.specialist', 'doctor.hospital'])
+            ->firstOrFail();
     }
 }
