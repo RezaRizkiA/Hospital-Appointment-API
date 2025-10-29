02b76fbd15b638fac451fb5b78163b93a849d89a
RezaRizkiA <rezariski.akbar@gmail.com>
2025-10-29 09:20:58 +0700

    feat(auth): membuat method login, loginToken, logout di AuthController dan juga membuat form request: RegisterRequest dan LoginRequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\AuthRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Services\AuthService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -15,14 +18,37 @@ class AuthController extends Controller
         $this->authService = $authService;
     }
 
-    public function register(AuthRequest $request)
+    public function register(RegisterRequest $request)
     {
         $validated = $request->validated();
         $user = $this->authService->register($validated);
 
         return response()->json([
-            'message' => 'Registration successful',
+            'message' => 'User Registered Successfully',
             'user' => new UserResource($user)
         ], 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        return $this->authService->login($validated);
+    }
+
+    public function tokenLogin(LoginRequest $request)
+    {
+        $validated = $request->validated();
+        return $this->authService->tokenLogin($validated);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
     }
 }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -4,14 +4,14 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class AuthRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,12 @@ class AuthRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|max:20|confirmed',
+            'photo' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'phone' => 'required|string|max:15',
+            'gender' => 'required|string|max:10'
         ];
     }
 }
